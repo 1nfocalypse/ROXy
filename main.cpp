af7f3821f98997f77203d0dfdf44a0858deb4fbf
The opposite of boredom is not pleasure, but excitement <109259299+1nfocalypse@users.noreply.github.com>
2024-06-04 21:52:25 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,46 +1,44 @@
 #include <iostream>
+#include <cstdint>
 #include <string>
+#include <vector>
+
+/*
+ * XOR scheme - CLEAR XOR KEY1 -> CIPHER
+ * CIPHER XOR CLEAR2 -> KEY2
+ * PUBLISH CIPHER, KEY2
+ *
+ * Asymmetric Scheme -> see NSA book
+ *
+ *
+ *
+ */
+
+// forward declarations
+std::string strXOR(std::string str1, std::string str2) noexcept; // allows for bitwise XOR of diff strings
+uint32_t customHash(uint32_t num); // 32-bit hash, collisions possible but shouldn't be important
+std::string iterativeHash(std::string key); // allows for n-length padding from PRNG
+// mersenne twister? Blum blum shub? should prolly do BBS
+std::string constructSet(int s, int k); // constructs translucent set for asymmetric system
+uint32_t blumblushub(uint32_t p1, uint32_t p2, uint32_t seed, uint32_t iterations); // CRNG
+// maybe a global MT?
+
 
 
 int main() {
-    while (1) {
-        std::string menChoiceProxy;
-        std::cout << "    _   __     _ __  __             _  ______  ____ " << std::endl;
-        std::cout << "   / | / ___  (_/ /_/ /_  ___  ____| |/ / __ \\/ __ \\" << std::endl;
-        std::cout << "  /  |/ / _ \\/ / __/ __ \\/ _ \\/ ___|   / / / / /_/ /" << std::endl;
-        std::cout << " / /|  /  __/ / /_/ / / /  __/ /  /   / /_/ / _, _/ " << std::endl;
-        std::cout << "/_/ |_/\\___/_/\\__/_/ /_/\\___/_/  /_/|_\\____/_/ |_|  " << std::endl;
-        std::cout << "********************************************************" << std::endl;
-        std::cout << "1. Encrypt" << std::endl;
-        std::cout << "2. Decrypt" << std::endl;
-        std::cout << "3. Quit" << std::endl;
-        std::cout << "********************************************************" << std::endl;
-        std::getline(std::cin, menChoiceProxy);
-        int menChoice = menChoiceProxy[0] - '0';
-        while (menChoice < 1 || menChoice > 3) {
-            std::cout << "Invalid choice. Please select a valid option." << std::endl;
-            std::cout << "********************************************************" << std::endl;
-            std::cout << "1. Encrypt" << std::endl;
-            std::cout << "2. Decrypt" << std::endl;
-            std::cout << "3. Quit" << std::endl;
-            std::cout << "********************************************************" << std::endl;
-            std::getline(std::cin, menChoiceProxy);
-            menChoice = menChoiceProxy[0] - '0';
-        }
-        switch (menChoice) {
-        case 1:
-            mySim.run();
-            break;
-        case 2:
-            mySim.configure();
-            break;
-        case 3:
-            std::cout << "Quitting..." << std::endl;
-            return 0;
-        default:
-            std::cout << "Unexpected Error: Uncaught menu input. Exiting..." << std::endl;
-            return 1;
-        }
-    }
-	return 0;
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+
+// ok lets build a construction of a translucent set
+
+std::string constructSet(uint32_t s, uint32_t k) {
+    uint32_t t = s + k;
+    std::vector<std::string> x;
+    
+}
+
+uint32_t blumblumshub(uint32_t p1, uint32_t p2, uint32_t seed, uint32_t iterations) {
+
 }
