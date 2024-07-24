f76723b201e23e66a136011e8bcf1dc7223c7edf
The opposite of boredom is not pleasure, but excitement. <109259299+1nfocalypse@users.noreply.github.com>
2024-07-24 07:32:53 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <queue>
 #include <bitset>
 #include <algorithm>
+#include <cstdlib>
+#include <time.h>
 
 /*
  *     ____  ____ _  __     
@@ -29,9 +31,18 @@
 
 
 /* TO DO
- * [ ] Finish construct translucent set (ASYMM SCHEME)
- * [ ] Implement encrypt/decrypt (ASYMM SCHEME)
  * [ ] TEST
+ *  - Symm
+ *  - Encryption *seems* to work, if decryption seems plausible check again.
+ *  - Fails on symm true decryption
+ *  - Untested on symm false decryption
+ *  - A bit clunky
+ *  - Asymm
+ *  - Untested
+ * [ ] Refactor
+ * [ ] TEST 2
+ * [ ] Write README
+ * [ ] Push
  */
 
 
@@ -43,13 +54,18 @@ void asymmEncrypt(); // encrypts with asymmetric encryption via translucent sets
 void symmDecrypt(); // decrypts with symmetric encryption via XOR
 void asymmDecrypt(); // decrypts with asymmetric encryption via translucent sets
 uint32_t customHash(uint32_t num); // used for efficient 32-bit uint seed generation
+uint32_t invertBBS(uint32_t prev, uint32_t p, uint32_t q); // inverts the current value of x0 via trapdoor permutation
 std::string iterativeHash(std::string key, uint32_t tarlen); // pads a seed to tarlen bytes
 std::string strXOR(std::string x, std::string y); // bitwise XOR of two n-len bitstrings
 std::string strToBin(std::string str); // convert a string to binary representation
 std::string binToStr(std::string str); // convert a bitstring to characters
-std::string constructTranslucentSet(uint32_t s, uint32_t k); // constructs translucent set for asymmetric system
+std::string constructTranslucentElement(); // constructs element of translucent set for asymmetric system
+std::string randomAsymmElement(); // returns a pseudorandom non-translucent 64 bit number as a bitstring
 std::vector<uint32_t> blumblumshub(uint32_t p1, uint32_t p2, uint32_t seed, uint32_t iterations); // CRNG
 bool isPrime(uint32_t num); // primality test for BBS verification
+bool bbsPredicate(uint32_t number); // hardcore predicate for blumblumshub, defined as a sum over GF2 of all elements.
+bool isTranslucentElement(std::string bitstr, uint32_t p, uint32_t q); // determines if a 64 bit bitstring is an element of St, returning 1 if it is, and 0 otherwise.
+// remember, this scheme is sender deniable, not receiver deniable. The receiver will always decode to the original text, presuming a bitflip did not occur.
 
 // ctor
 // PRE: Program starts
@@ -239,6 +255,7 @@ void symmEncrypt() {
     else if (key.length() != line.length()) {
         key = iterativeHash(key, line.length());
     }
+    std::string asciiKey = key;
     key = strToBin(key);
     line = strToBin(line);
     std::string ciphertext = strXOR(key, line);
@@ -266,14 +283,14 @@ void symmEncrypt() {
         return;
     }
     // perform XOR operation to yield decoy key
-    if (decoy.length() > ciphertext.length()) {
+    if (decoy.length() > asciiKey.length()) {
         std::cout << "Decoy too long! Returning to menu." << std::endl;
         return;
     }
-    else if (decoy.length() < ciphertext.length()) {
+    else if (decoy.length() < asciiKey.length()) {
         std::cout << "Proceeding with undersized decoy." << std::endl;
         std::cout << "Note: Decoy will be padded out with spaces to meet length requirements." << std::endl;
-        while (decoy.length() < ciphertext.length()) {
+        while (decoy.length() < asciiKey.length()) {
             decoy += " ";
         }
     }
@@ -350,7 +367,7 @@ void symmDecrypt() {
         keyRaw.close();
         return;
     }
-    std::string key;
+    // std::string key;
     // First key is legitimate, second is for the decoy.
     if (menChoice == 1) {
         // read in real key
@@ -386,7 +403,48 @@ void symmDecrypt() {
 // WARNINGS:
 // STATUS:
 void asymmEncrypt() {
-
+    std::string path;
+    std::string outfileName;
+    std::string line;
+    std::ifstream rawFile;
+    std::ofstream cryptoOut;
+    std::cout << "Please enter the path to the file you would like encrypted." << std::endl;
+    std::cout << "++++++++++++++++++++++++++++++++++++++++++++++++++++++++" << std::endl;
+    std::getline(std::cin, path);
+    std::cout << "Please enter the name of the output file:" << std::endl;
+    std::cout << "++++++++++++++++++++++++++++++++++++++++++++++++++++++++" << std::endl;
+    std::getline(std::cin, outfileName);
+    rawFile.open(path.c_str());
+    if (rawFile.good()) {
+        auto out = std::string();
+        auto buf = std::string(100, '\0');
+        while (rawFile.read(&buf[0], 100)) {
+            out.append(buf, 0, rawFile.gcount());
+        }
+        out.append(buf, 0, rawFile.gcount());
+        line = out;
+        rawFile.close();
+    }
+    std::string bitstr;
+    for (uint32_t i = 0; i < line.length(); ++i) {
+        bitstr += std::bitset<8>(line[i]).to_string();
+    }
+    std::string ciphertext;
+    for (uint32_t i = 0; i < bitstr.length(); ++i) {
+        if (bitstr[i] == '1') {
+            ciphertext += constructTranslucentElement();
+        }
+        else {
+            ciphertext += randomAsymmElement();
+        }
+    }
+    cryptoOut.open(outfileName);
+    cryptoOut << ciphertext;
+    cryptoOut.close();
+    std::cout << "Successfully wrote data." << std::endl;
+    std::cout << "Ciphertext written to: " << outfileName << std::endl;
+    std::cout << "Retain original ciphertext outfile. Otherwise, data loss may occur." << std::endl;
+    std::cout << "++++++++++++++++++++++++++++++++++++++++++++++++++++++++" << std::endl;
 }
 
 // asymmDecrypt()
@@ -395,7 +453,46 @@ void asymmEncrypt() {
 // WARNINGS:
 // STATUS:
 void asymmDecrypt() {
-
+    uint32_t p = 6827, q = 4079;
+    std::string path;
+    std::string outfileName;
+    std::string line;
+    std::ifstream rawFile;
+    std::ofstream clearOut;
+    std::cout << "Please enter the path to the file you would like decrypted." << std::endl;
+    std::cout << "--------------------------------------------------------" << std::endl;
+    std::getline(std::cin, path);
+    std::cout << "Please enter the name of the output file:" << std::endl;
+    std::cout << "--------------------------------------------------------" << std::endl;
+    std::getline(std::cin, outfileName);
+    rawFile.open(path.c_str());
+    if (rawFile.good()) {
+        auto out = std::string();
+        auto buf = std::string(100, '\0');
+        while (rawFile.read(&buf[0], 100)) {
+            out.append(buf, 0, rawFile.gcount());
+        }
+        out.append(buf, 0, rawFile.gcount());
+        line = out;
+        rawFile.close();
+    }
+    std::string bitBuffer, out;
+    for (uint32_t i = 0; i < (bitBuffer.length() / 64); ++i) {
+        bitBuffer = line.substr(i * 64, 64);
+        if (isTranslucentElement(bitBuffer, p, q)) {
+            out += '1';
+        }
+        else {
+            out += '0';
+        }
+    }
+    std::string outText = binToStr(out);
+    std::cout << "Writing cleartext to " << outfileName << "..." << std::endl;
+    clearOut.open(outfileName);
+    clearOut << outText;
+    clearOut.close();
+    std::cout << "Data written." << std::endl;
+    std::cout << "--------------------------------------------------------" << std::endl;
 }
 
 // customHash(int32_t num)
@@ -477,27 +574,92 @@ std::string strToBin(std::string str) {
 // PRE: A string of bits is passed
 // POST: A char converted string is returned
 // WARNING: Invalid data types may be corrupted via this process.
-// STATUS: Completed, untested
+// STATUS: Completed, error
 std::string binToStr(std::string str) {
     std::string retStr;
     while (str.length() > 8) {
+        std::string testing = str.substr(0, 8);
         int num = std::stoi(str.substr(0, 8), 0, 2);
         char myChar = static_cast<char>(num);
         retStr += myChar;
-        str = str.substr(9, str.length());
+        str = str.substr(8, str.length());
     }
     return retStr;
 }
 
-// constructTranslucentSet(uint32_t s, uint32_t k)
+// constructTranslucentElement()
 // PRE: Two unsigned integers passed denoting size
 // POST: A set is constructed per Canetti et. al. construction 2
 // WARNING: 
 // STATUS: Incomplete, untested
-std::string constructTranslucentSet(uint32_t s, uint32_t k) {
-    uint32_t t = s * k;
+// COMMENTS: Using BBS to obtain trapdoor function, based on Rabin's Quadratic Residue Assumption. Since BBS can be utilized as a OWF,
+// we are able to establish a hardcore predicate of the form
+// n:= pq | p, q equiv 3 % 4
+// set X of int % n coprime to n && are quadratic residues (a value x : E int y : y^2 = x % n)
+// A quadratic residue coprime to n has 4 distrinct square roots, and exactly one of them is itself a quadratic residue.
+// Def F: x -> x^2 % n. E qudratic residue x : f(x) = y.
+// Define b(x) to be the XOR of all bits of the binary rep of x. Returns bool value. Given x, computation is hence trivial. Given f(x), computation is at least as hard as factoring n.
+// Now i just need to define the trapdoor function from Rabin's theorem.
+// Given n := pq | p, q equiv 3 % 4
+// Compute z given a : a = z^2 % n. Trapdoor is factorization of n. W/ trapdoor, sol of z given as cx + dy, cx - dy, -cx + dy, - cx - dy
+// where a equiv x^2 mod p, a = y^2 % q, c equiv 1 % p, c equiv 0 % q, d = 0 % p, d equiv 1 % q.
+std::string constructTranslucentElement() {
+    uint32_t t = 64;
+    uint32_t s = 32, k = 32; // P(0 dec as 1) = 1 / 2^32 apprx .000000000232, 2 bits/10 billion, approx 1 bitflip per 625 MB is E
+    uint32_t p = 6827, q = 4079;
+    // done to illustrate BBS functionality - in reality, sender will only have n : n := p * q. In this toy, p,q are set.
+    // In practicum, users should use p,q of cryptographic size (256/512 bits), along with a different seeding algorithm)
+    uint32_t seed = customHash(time(0));
+    std::vector<uint32_t> randNum;
+    std::vector<bool> predicates;
+    // std::vector<bool> x0;
+    std::vector<bool> ciphertext;
+    // generate full element of translucent set
+    for (uint32_t i = 0; i < k; ++i) {
+        if (i != 0) {
+            seed = randNum[0];
+        }
+        randNum[0] = (blumblumshub(p, q, seed, 1))[0]; // generate initial x_0
+        predicates.push_back(bbsPredicate(randNum[0]));
+    }
+    std::string x0 = std::bitset<32>(randNum[0]).to_string();
+    std::string predicateStr;
+    for (uint32_t i = 0; i < k; ++i) {
+        predicateStr += static_cast<char>(predicates[i]);
+    }
+    std::string bitstring = x0 + predicateStr;
+    return bitstring;
+}
 
-    return "";
+std::string randomAsymmElement() {
+    uint32_t seed = time(0);
+    uint32_t left, right;
+    std::string retstr;
+    left = customHash(seed);
+    right = customHash(left);
+    std::string strseed, strleft, strright, newl, newr;
+    strleft = std::bitset<32>(left).to_string();
+    strright = std::bitset<32>(right).to_string();
+    strseed = std::bitset<32>(seed).to_string();
+    newl = strXOR(strright, strXOR(strleft, strseed));
+    newr = strleft;
+    strleft = newl;
+    strright = newr;
+    retstr = strleft + strright;
+    return retstr;
+}
+
+// pq = n, trapdoor is BBS. p,q, retained as trapdoor information d.
+// defined B : [0, 1]^s -> [0,1]
+bool bbsPredicate(uint32_t number) {
+    std::string num = std::bitset<32>(number).to_string();
+    int parity = 0;
+    for (size_t i = 0; i < num.length(); ++i) {
+        if (num[i] == '1') {
+            parity = (parity) + 1 % 2;
+        }
+    }
+    return static_cast<bool>(parity);
 }
 
 // blumblumshub(uint32_t p1, uint32_t p2, uint32_t seed, uint32_t iterations)
@@ -556,3 +718,95 @@ bool isPrime(uint32_t num) {
     }
     return true;
 }
+
+bool isTranslucentElement(std::string bitstr, uint32_t p, uint32_t q) {
+    // hmm
+    // how do i wanna try and accomplish this
+    // obviously, we can just run a predicate on the current x_0
+    // if the predicate yields the correct predicate, then we invert
+    // if at any point the predicate does not match, then we reject as
+    // an element
+    // first isolate the predicate values, then flip them so the most recent is evaluated first
+    std::string predicatesStr = bitstr.substr(32, 32);
+    std::string x0 = bitstr.substr(0, 32);
+    std::reverse(predicatesStr.begin(), predicatesStr.end());
+    bool predicates[32];
+    for (uint32_t i = 0; i < 32; ++i) {
+        if (predicatesStr[i] == '1') {
+            predicates[i] = true;
+        }
+        else {
+            predicates[i] = false;
+        }
+    }
+    long int signedTmp = std::stol(x0, nullptr, 2);
+    uint32_t unsInt = static_cast<uint32_t>(signedTmp);
+    for (uint32_t i = 0; i < 32; ++i) {
+        if (bbsPredicate(unsInt) == predicates[i]) {
+            unsInt = invertBBS(unsInt, p, q);
+        }
+        else {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+uint32_t invertBBS(uint32_t prev, uint32_t p, uint32_t q) {
+    // note: p = 6827, q = 4079;
+    // in order to find z :given a (ciphertext prev), find a equiv z^2 % n, find
+    // cx + dy
+    // cx - dy
+    // -cx + dy
+    // -cx - dy
+    // where:
+    /*
+    a equiv x^2 % p
+    a equiv y^2 % q
+    c equiv 1 mod p
+    c equiv 0 mod q
+    d equiv 0 mod p
+    d equiv 1 mod q
+    (via Chinese Remainder Theorem)
+    given primes p, q we find 
+    x equiv a^((p+1)/4) mod p
+    y equiv a^((q+1)/4) mod q
+    */
+    // so first we want to solve for x, y
+    uint32_t xpow = (p + 1) / 4, ypow = (q + 1) / 4;
+    uint32_t prevTmp, x, y;
+    for (uint32_t i = 0; i < xpow; ++i) {
+        x = (prev * prev) % p;
+    }
+    for (uint32_t i = 0; i < ypow; ++i) {
+        y = (prev * prev) % q;
+    }
+    // now we need to enumerate c,d
+    uint32_t c = 1, d = 1;
+    while (((c % p) != 1) && ((c % q) != 0)) {
+        ++c;
+    }
+    while (((d % q) != 1) && ((d % p) != 0)) {
+        ++d;
+    }
+    long int potentials[4];
+    uint32_t inversion = 0;
+    potentials[0] = (c * x) + (d * y);
+    potentials[1] = (c * x) - (d * y);
+    potentials[2] = ((-1 * c) * x) + (d * y);
+    potentials[3] = ((-1 * c) * x) - (d * y);
+    for (int i = 0; i < 4; ++i) {
+        if (potentials[i] > 0) {
+            long int guessedRoot = potentials[i] / potentials[i];
+            if ((guessedRoot * guessedRoot) == potentials[i]) {
+                inversion = potentials[i];
+                break;
+            }
+        }
+    }
+    if (inversion == 0) {
+        return 0;
+    }
+    return inversion;
+}  
